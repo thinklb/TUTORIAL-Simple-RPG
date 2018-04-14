2f245614a7d323b8fb86c0bc76ecb7fc78ef0857
Leo Basanez <leo@khanacademy.org>
2018-04-14 16:10:05 -0700

    Initial commit

---
+++ Assets/Scripts/WorldInteraction.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldInteraction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        // When left mouse button is pressed, 
+        if (Input.GetMouseButtonDown(0) && !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject())
+        {
+            GetInteraction();
+        }
+	}
+
+    // 
+    void GetInteraction ()
+    {
+        Ray interactionRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit interactionInfo;
+
+        if (Physics.Raycast(interactionRay, out interactionInfo, Mathf.Infinity))
+        {
+            GameObject interactedObject = interactionInfo.collider.gameObject;
+            if (interactedObject.tag == "Interactable Object")
+            {
+                Debug.Log("Interactable interacted.");
+            }
+            else
+            {
+                // Player movement
+
+            }
+        }
+    }
+}
